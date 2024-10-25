2bfd5ae3e8fde10547f554dee24d33f70126edea
ZiyodullohDilshodov <dilshodovziyodillo@gmail.com>
2024-10-26 01:37:25 +0500

    Fix  services

--- EffectiveMobile.Data/Migrations/20241025194442_EMigration.Designer.cs
+++ EffectiveMobile.Data/Migrations/20241025194442_EMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EffectiveMobile.Data.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EffectiveMobile.Data.Migrations
 {
     [DbContext(typeof(EffectiveMobileDbContext))]
-    partial class EffectiveMobileDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241025194442_EMigration")]
+    partial class EMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
